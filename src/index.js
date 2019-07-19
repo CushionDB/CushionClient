fc13ec97097c4e5a1d4703ec7aca690044994ff3
Avshar K <avshark@gmail.com>
2019-07-19 13:10:01 -0400

    Cushion Global Object Test
    Co-authored-by: Jaron Truman <joltruman88@gmail.com>
    Co-authored-by: Daniel Rote <drote.email@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import Cushion from './cushion';
-// import './cushion';
+//
+global.cushion = new Cushion({localDBName:'test',username:'foo',remoteBaseURL:'http'});
 
-window.a = 123;
-
-let st = new Cushion();
-if (st) console.log('defined');
-
-class Person {
-  constructor(name){
-    this.name = name;
-  }
-  getName(){
-    return this.name;
-  }
-}
-
-let p = new Person('bla');
-console.log(p.getName());
-if (Cushion) console.log('cushion imported');
-console.log(a);
